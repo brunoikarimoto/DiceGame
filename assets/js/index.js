1391f469c7e210c4a3fb6d1892ca9a87133eaec0
Bruno Ikarimoto <bruno.ikarimoto@outlook.com>
2023-07-14 20:21:43 -0300

    dice game

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+let btn = document.querySelector('button');
+let dices = document.querySelectorAll('.player img');
+let title = document.querySelector('h1');
+
+btn.addEventListener('click', () => {
+    let randNum1 = Math.floor(Math.random() * 6) + 1;
+    let randNum2 = Math.floor(Math.random() * 6) + 1;
+
+    dices[0].setAttribute('src', './assets/img/dice' + randNum1 + ".png");
+    dices[1].setAttribute('src', './assets/img/dice' + randNum2 + ".png");
+
+    if(randNum1 > randNum2){
+        title.innerHTML = "Player 1 Wins!!";
+    }
+    else if(randNum1 < randNum2){
+        title.innerHTML = "Player 2 Wins!!";
+    }
+    else{
+        title.innerHTML = "Draw";
+    }
+});
