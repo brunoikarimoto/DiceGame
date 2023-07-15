040e4090ac149bcbeb8e7a123b6bb0f66b615598
Bruno Ikarimoto <bruno.ikarimoto@outlook.com>
2023-07-14 21:09:18 -0300

    alt

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,9 @@ btn.addEventListener('click', () => {
     let randNum2 = Math.floor(Math.random() * 6) + 1;
 
     dices[0].setAttribute('src', './assets/img/dice' + randNum1 + ".png");
+    dices[0].setAttribute('alt', randNum1);
     dices[1].setAttribute('src', './assets/img/dice' + randNum2 + ".png");
+    dices[1].setAttribute('alt', randNum2);
 
     if(randNum1 > randNum2){
         title.innerHTML = "Player 1 Wins!!";
